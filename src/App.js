7cb1c5486ba018b027e80c41defb94a42fef13ec
Katherine Aguilera <katherinam35@gmail.com>
2019-07-24 13:03:37 -0500

    Styles Globales

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Category } from './components/Category'
-
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './GlobalStyles'
 export const App = () => (
-  <Category />
+  <div>
+    <GlobalStyles />
+    <ListOfCategories />
+  </div>
 )
