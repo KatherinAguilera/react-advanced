5e98522f520883a2de213db15ad54f814a44d35b
Katherine Aguilera <katherinam35@gmail.com>
2019-07-25 16:51:21 -0500

    Hooks useState useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+
 export const ListOfCategories = () => {
+  // Hooks
+  const [categories, setCategories] = useState([])
+
+  // Se ejecuta cada vez que se redenderice
+  useEffect(function () {
+    window.fetch('https://petgram-server.midudev.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
-        categories.map(category =>
-          <Item key={category.id}>
-            <Category {...category} /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
