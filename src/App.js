3ad4f1bab75c63bcbd932e657345f70417256c80
Katherine Aguilera <katherinam35@gmail.com>
2019-07-29 16:59:01 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { GlobalStyles } from './components/styles/GlobalStyles'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar/index'
 
 import { Home } from './pages/Home'
+
 import { Detail } from './pages/Detail'
-import { NavBar } from './components/NavBar/index'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -17,6 +25,20 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
