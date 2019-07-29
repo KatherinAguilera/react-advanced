87faec37d17eca233f35092c6b041d82637064a1
Katherine Aguilera <katherinam35@gmail.com>
2019-07-29 10:24:42 -0500

    Parámetros para un query con GraphQL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './components/styles/GlobalStyles'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+
 export const App = () => (
   <div>
     <GlobalStyles />
     <Logo />
     <ListOfCategories />
-    <ListOfPhotoCards />
+    <ListOfPhotoCards categoryId={1} />
   </div>
 )
