7160ae1c24109026727ef870da61ba894953be73
Katherine Aguilera <katherinam35@gmail.com>
2019-07-29 17:14:40 -0500

    Introducción a React.Context

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Router } from '@reach/router'
-
-const UserLogged = ({ children }) => {
-  return children({ isAuth: true })
-}
+import Context from './Context'
 
 export const App = () => {
   return (
@@ -25,7 +22,8 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
-      <UserLogged>
+
+      <Context.Consumer>
         {
           ({ isAuth }) =>
             isAuth
@@ -38,7 +36,8 @@ export const App = () => {
                 <NotRegisteredUser path='/user' />
               </Router>
         }
-      </UserLogged>
+      </Context.Consumer>
+
       <NavBar />
     </div>
   )
