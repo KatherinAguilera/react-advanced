fea184d1b44150611386e4e55ccca4020d0fa65e
Katherine Aguilera <katherinam35@gmail.com>
2019-07-29 12:39:17 -0500

    Usando las mutaciones con los likes

---
+++ src/components/FavButton/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  padding-top: 8px;
+  & svg {
+    margin-right: 4px;
+    border-radius: 50%;
+    border: solid  #E68F7D;
+    padding:3px;
+    color: #DE530D;
+  }
+`
