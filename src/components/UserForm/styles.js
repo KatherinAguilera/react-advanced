9f2c2d9dd53fb914091b4781018303ccb854c375
Katherine Aguilera <katherinam35@gmail.com>
2019-08-04 13:47:34 -0500

    Cerrar sesión

--- src/components/UserForm/styles.js
+++ src/components/UserForm/styles.js
@@ -15,20 +15,6 @@ export const Input = styled.input`
     opacity: .3;
   }
 `
-
-export const Button = styled.button`
-  background: #fc9480;
-  border-radius: 3px;
-  color: #fff;
-  height: 32px;
-  display: block;
-  width: 100%;
-  text-align: center;
-  &[disabled] {
-    opacity: .3;
-  }
-`
-
 export const Title = styled.h2`
   font-size: 16px;
   font-weight: 500;
